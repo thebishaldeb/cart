401da5ddacf32c6743e6d7fc8166f2a83487456a
Bishal Deb <bishaldeb656@gmail.com>
2019-03-17 21:09:05 +0530

    Reducer and actions of cart updated.

--- src/reducers/cartReducer.js
+++ src/reducers/cartReducer.js
@@ -1,8 +1,7 @@
 "use strict";
 import {
   ADD_TO_CART,
-  DELETE_FROM_CART,
-  UPDATE_ITEM_UNITS
+  DELETE_FROM_CART
 } from "../actions/cartActions";
 
 export default function cartReducer(state = [], action = {}) {
@@ -10,18 +9,10 @@ export default function cartReducer(state = [], action = {}) {
     case ADD_TO_CART:
       let existingIndex = findProductIndex(state, action.payload.id);
       if (existingIndex !== -1) {
-        state[existingIndex].units += 1;
+        state[existingIndex].units = 1;
         return state.concat([]);
       }
       return state.concat(action.payload);
-
-    case UPDATE_ITEM_UNITS:
-      let existingItemIndex = findProductIndex(state, action.payload.id);
-      if (state[existingItemIndex].units === 0 && action.payload.units === -1) {
-        break;
-      }
-      state[existingItemIndex].units += action.payload.units;
-      return state.concat([]);
 
     case DELETE_FROM_CART:
       let indexToDel = findProductIndex(state, action.payload.id);
