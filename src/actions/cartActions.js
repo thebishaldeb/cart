7940dfec36e5328eb26ec6124aa1147cb5c2ac6a
Bishal Deb <bishaldeb656@gmail.com>
2019-03-17 22:05:07 +0530

    Checkout steps added.

--- src/actions/cartActions.js
+++ src/actions/cartActions.js
@@ -2,10 +2,19 @@
 export const ADD_TO_CART = "ADD_TO_CART";
 export const DELETE_FROM_CART = "DELETE_FROM_CART";
 
-export function addToCart({ id, title, description, price, units = 1 }) {
+export function addToCart({
+  id,
+  title,
+  description,
+  rent,
+  image,
+  refund,
+  days,
+  date
+}) {
   return {
     type: ADD_TO_CART,
-    payload: { id, title, description, price, units }
+    payload: { id, title, description, rent, image, refund, days, date }
   };
 }
 export function deleteFromCart({ id }) {
--- src/components/body/cart.js
+++ src/components/body/cart.js
@@ -2,8 +2,10 @@
 import React from "react";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
-import { deleteFromCart, updateItemUnits } from "../../actions/cartActions";
-import { Row, Col, Button } from "antd";
+import { deleteFromCart } from "../../actions/cartActions";
+import { Row, Col, Button, Steps } from "antd";
+
+const Step = Steps.Step;
 
 class Cart extends React.Component {
   renderCart() {
@@ -16,14 +18,6 @@ class Cart extends React.Component {
   handleDeleteFromCart(id) {
     this.props.deleteFromCart({ id });
   }
-  handleDeductUnit(id) {
-    let units = -1;
-    this.props.updateItemUnits({ id, units });
-  }
-  handleAddUnit(id) {
-    let units = 1;
-    this.props.updateItemUnits({ id, units });
-  }
 
   cartList() {
     return this.props.cart.map(cartItem => {
@@ -31,8 +25,6 @@ class Cart extends React.Component {
         <CartItem
           key={cartItem.id}
           cartItem={cartItem}
-          onAddUnit={this.handleAddUnit.bind(this, cartItem.id)}
-          onDeductUnit={this.handleDeductUnit.bind(this, cartItem.id)}
           handleDeleteFromCart={this.handleDeleteFromCart.bind(
             this,
             cartItem.id
@@ -68,10 +60,21 @@ class Cart extends React.Component {
   render() {
     if (this.props.cart.length !== 0) {
       return (
-        <aside className="cart">
-          {this.renderCart()}
-          {this.cartTotal()}
-        </aside>
+        <div className="payment">
+          <div className="checkout">Checkout</div>
+          <Steps className="steps" current={0}>
+            <Step title="Cart" />
+            <Step title="Billing Address" />
+            <Step title="Payment options" />
+            <Step title="Order Status" />
+          </Steps>
+          <div className="cart">
+            {this.renderCart()}
+          </div>
+          <div>
+            {this.cartTotal()}
+          </div>
+        </div>
       );
     }
 
@@ -87,20 +90,14 @@ class CartItem extends React.Component {
           <Col xs={12} sm={6}>
             <h5>
               {this.props.cartItem.title}{" "}
-              <div pullRight>Price: INR {this.props.cartItem.price}</div>
+              <div pullRight>Price: INR {this.props.cartItem.rent}</div>
             </h5>
           </Col>
           <Col xs={6} sm={4}>
             <p>
-              units :&nbsp;
-              <div bsStyle="success"> {this.props.cartItem.units} </div>
+              No of days :&nbsp;
+              <div bsStyle="success"> {this.props.cartItem.days} </div>
               &nbsp;
-              <Button bsSize="small" onClick={() => this.props.onAddUnit()}>
-                +
-              </Button>
-              <Button bsSize="small" onClick={() => this.props.onDeductUnit()}>
-                -
-              </Button>
             </p>
           </Col>
           <Col xs={6} sm={2}>
@@ -126,8 +123,7 @@ function mapStateToProps(state) {
 function mapActionsToProps(dispatch) {
   return bindActionCreators(
     {
-      deleteFromCart,
-      updateItemUnits
+      deleteFromCart
     },
     dispatch
   );
